5bb4836ae1dc602467a831b7e19c42334f84e039
janokular <janekokularczyk@gmail.com>
2025-01-30 13:41:11 +0100

    init

---
+++ multi-pull.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This script pulls changes for every repository from a list
+
+REPO_FILE='./repos'
+
+# Check if REPO_FILE exists and is a file
+if [[ ! -e "${REPO_FILE}" ]]
+then
+  echo "Cannot open ${REPO_FILE}" >&2
+  exit 1
+fi
+
+# Pull changes for every repository from the REPO_FILE
+for REPO in $(cat "${REPO_FILE}")
+do
+  REPO=$(realpath ${REPO})	
+  echo "Pulling changes for ${REPO}"
+  git -C "${REPO}" pull
+  
+  # Check the status of the git pull command
+  if [[ "${?}" -ne 0 ]]
+  then
+    echo -e "Failed at pulling changes for ${REPO}\n"
+  else
+    echo -e "Successfully pulled changes for ${REPO}\n"
+  fi
+done
