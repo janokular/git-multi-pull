803fd8e01b163bbdca5f07e470a9544f5e991aed
janokular <janekokularczyk@gmail.com>
2026-04-27 17:33:05 +0200

    staged changes

--- multi-pull.sh
+++ multi-pull.sh
@@ -10,41 +10,61 @@ green="\033[0;32m"
 reset="\033[0m"
 
 usage() {
-  echo "Usage: ${0} [-v] [-f FILEPATH]"
+  echo "Usage: ${0} [-f FILEPATH] [-u] [-v]"
   echo "Pull changes for all listed repositories: default ${repo_file}"
   echo -e "-f FILEPATH\tUse external file with list of repositories"
+  echo -e "-u\t\tUpdate ${repo_file} with repositories in ${repo_path}"
   echo -e "-v\t\tVerbose mode"
   exit 1
 }
 
 # Check options provided by the user
-while getopts f:v option &> /dev/null; do
+while getopts f:uv option &> /dev/null; do
   case ${option} in
     f) repo_file=${OPTARG} ;;
+    u) update_repos='true' ;;
     v) verbose_mode='true' ;;
     ?) usage ;;
   esac
 done
 
-# Check if repo_file exists and is a file
-if [[ ! -f "${repo_file}" ]]; then
-  echo "Cannot open ${repo_file}" >&2
-  exit 1
+# Update repo_file
+if [[ ${update_repos} = 'true' ]]; then
+  tmp_file=$(mktemp)
+  basename -a $(ls -d ${repo_path}/*/) > $tmp_file
+  
+  # Check if repos_file exists and if it has to be updated
+  if [[ -f $repo_file ]] \
+  && diff $tmp_file $repo_file &> /dev/null; then
+    echo -e "${yellow}Already up to date: ${repo_file}${reset}\n"
+  else
+    cat $tmp_file > $repo_file
+
+    # Check the status of the git pull command
+    if [[ "${?}" -ne 0 ]]; then
+      echo -e "${red}Failed at updating ${repo_file}${reset}\n"
+    else
+      echo -e "${green}Successfully updated ${repo_file}${reset}\n"
+    fi
+  fi
+
+  rm $tmp_file
 fi
 
+# Check if repo_file exists and is a file
+if [[ ! -f "${repo_file}" ]]; then
+  echo "Provided file ${repo_file} deos not exist" >&2
+  exit 1
 # Check if repo_file is not empty
-if [[ ! -s "${repo_file}" ]]; then
+elif [[ ! -s "${repo_file}" ]]; then 
   echo "Provided file ${repo_file} is empty" >&2
   exit 1
 fi
 
 # Pull changes for every repository from the repo_file
 for repo in $(cat "${repo_file}"); do
-  base_repo=$(basename ${repo})
-  
   # Check if user wants to get additional messages
   if [[ ${verbose_mode} = 'true' ]]; then
-    echo "Trying to pull changes for ${base_repo}"
     git -C "${repo_path}/${repo}" pull
   else
     git -C "${repo_path}/${repo}" pull &> /dev/null
@@ -52,9 +72,9 @@ for repo in $(cat "${repo_file}"); do
   
   # Check the status of the git pull command
   if [[ "${?}" -ne 0 ]]; then
-    echo -e "${red}Failed at pulling changes for ${base_repo}${reset}\n"
+    echo -e "${red}Failed at pulling changes for ${repo}${reset}\n"
   else
-    echo -e "${green}Successfully pulled changes for ${base_repo}${reset}\n"
+    echo -e "${green}Successfully pulled changes for ${repo}${reset}\n"
   fi
 done
 
